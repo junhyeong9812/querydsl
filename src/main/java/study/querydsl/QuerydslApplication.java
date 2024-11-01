47789f4103e1ea575472c87ad261114f5233ee34
jun <pickjog@gmail.com>
2024-11-01 17:12:38 +0900

    프로젝트 기본설정

--- src/main/java/study/querydsl/QuerydslApplication.java
+++ src/main/java/study/querydsl/QuerydslApplication.java
@@ -9,5 +9,9 @@ public class QuerydslApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
 	}
+	//기본적으로 build and running을 gradle이 아닌 intelij로 변경
+	//gradle설정에서 변경 가능
+	//lombok설정 및 Build>Compiler>Annotation Processors >Enable annotation processing 설정
+
 
 }
