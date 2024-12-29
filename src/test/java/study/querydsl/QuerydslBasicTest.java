ee504a67820ab200c1bfccdf8f5dd749e16ae4eb
jun <pickjog@gmail.com>
2024-12-29 13:35:49 +0900

    Q-type/검색 조건 쿼리/결과 조회

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
@@ -14,6 +15,9 @@ import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static study.querydsl.entity.QMember.member;
 
 @SpringBootTest
 @Transactional
@@ -50,7 +54,7 @@ public class QuerydslBasicTest {
                         " where m.username=:username", Member.class)
                 .setParameter("username", "member1")
                 .getSingleResult();
-        Assertions.assertThat(findMember.getUsername()).isEqualTo("member1");
+        assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 
     @Test
@@ -59,6 +63,22 @@ public class QuerydslBasicTest {
         //이렇게 기본적으로 JPAQueryFactory에 em을 줘야 하고
         //"m"은 별칭으로 나중에는 만들어져있는 이름을 사용.
         QMember m = new QMember("m");
+        //위처럼 별칭을 이용한 방식이 존재하며
+//        QMember member=QMember.member;
+        //이렇게 생성된 맴버 정보도 존재한다.
+        //이때 QMember에 대해서 스태틱 임포트로
+        Member findMeberStatic = queryFactory
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))//파라미터 바인딩
+                .fetchOne();
+        //위처럼 스태틱 임포트를 사용하면 import Static을 통해 바로 호출할 수 있다.
+        //이렇게 스태틱 임포트로 사용하는 것을 권장한다.
+        //결국 QueryDsl로 만드는건 JPQL이 되는 것인데 이때 jpql이 궁금할 경우
+        //use_sql_comments:true 옵션을 통해 ymi를 적용하면 jpql을 주석으로 나오는 것을 확인할 수 있다.
+        //member1이라고 나간 이유는 기본적으로 자동Q쿼리에서 member1로 생성하기 때문이다.
+        //하지만 위처럼 별칭을 통한 생성을 하면 jpql의 별칭이 m1으로 변경된다.
+        //같은 테이블을 조인해야 되는 경우에는 위처럼 별칭 선언을 해서 사용하면 된다.
 
         Member findMeber = queryFactory
                 .select(m)
@@ -72,10 +92,102 @@ public class QuerydslBasicTest {
         //런타임 오류를 통해 확인해야 된다.
         //QueryDsl을 사용하면 컴파일타임에 오류가 잡혀서 훨씬 간편하게 확인할 수 있다.
 
-        Assertions.assertThat(findMeber.getUsername()).isEqualTo("member1");
+        assertThat(findMeber.getUsername()).isEqualTo("member1");
 
     }
 
+    //검색 조건 쿼리
+    @Test
+    public void search(){
+        Member member1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+        assertThat(member1.getUsername()).isEqualTo("member1");
+    }
+    //JPQL이 제공하는 모든 검색 조건 제공
+    /*
+    .eq("member1") 앞의 값과 문자열이 일치
+    .ne("member1") 앞의 값과 문자열의 값이 일치하지 않을 경우
+    .eq("member1").not() 앞의 값과 문자열의 값이 일치하지 않을 경우
+    .isNotNull() 널이 아닌 값
+    .in(x,y) x,y를 포함하고 있는 데이터
+    notIn(x,y) x,y를 포함하고 있지 않는 데이터
+    between(10,30) 10에서 30사이의 데이터
+
+    goe(x) x보다 크거나 같다.
+    gt(x) x보다 크다
+    loe(x) x보다 작거나 같다
+    lt(x) x보다 작다
+
+    .like("x%") like검색
+    .contains("x") like %x%검색
+    .startsWith("x") like x%검색
+
+     */
+
+    @Test
+    public void searchAndParam(){
+        Member member1 = queryFactory
+                .selectFrom(member)
+                .where(
+//                        member.username.eq("member1")
+//                        .and(member.age.between(10, 30))
+                        member.username.eq("member1")
+                                ,(member.age.between(10, 30))
+                        //이처럼 위처럼 and를 통해 연쇄체이닝으로 가져가는 경우도 있지만
+                        //,를 통해 and가 생략되어 where절에 넣는 것도 가능하다.
+                        //그리고 ,로 처리하면 null인 경우 무시하기 때문에 동적쿼리를 만들 때 상당히 유효하다.
+                )
+                .fetchOne();
+        assertThat(member1.getUsername()).isEqualTo("mebere1");
+    }
+
+    /*Querydsl의 결과조회
+    fetch() :리스트 조회, 데이터 없으면 빈 리스트를 반환
+    fetchOne() : 단건 조회
+    -결과가 없으면 :null
+    -결과가 둘 이상이면 "NonUniqueResultException"
+    fetchFirst() : limit(1).fetchOne()
+    fetchResult() : 페이징 정보 포함, total count 쿼리 추가 실행
+    fetchCount() :count 쿼리로 변경해서 count 수 조회
+     */
+    @Test
+    public void resultFetch(){
+//        //리스트 조회
+//        List<Member> fetch = queryFactory
+//                .selectFrom(member)
+//                .fetch();
+//        //단일 조회
+//        Member fetchOne = queryFactory
+//                .selectFrom(member)
+//                .fetchOne();
+//
+//        //처음 값 조회
+//        Member fetchFirst = queryFactory
+//                .selectFrom(member)
+////                .limit(1)
+////                .fetchOne();
+//                .fetchFirst();
+
+
+        QueryResults<Member> results = queryFactory
+                        .selectFrom(member)
+                .fetchResults();
+        results.getTotal();
+        List<Member> content = results.getResults();
+        results.getLimit();
+        //이렇게 하면 쿼리가 두번 실행되는데 토탈카운트때문에 쿼리가 두번나가서 카운트쿼리까지 나간다.
+        //토탈이 있어야 총 페이지 수를 알려줄 수 있기 때문에 필요.
+
+        long total = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+        //이렇게 카운트 쿼리로 바꿔서 가져오도록 할 수 있다.
+
+
+    }
 
 }
 
